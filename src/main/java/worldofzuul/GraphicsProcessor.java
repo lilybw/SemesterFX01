190647088cafed4cdf500bab507a77d18921acee
lilybw <lilybwanscher@gmail.com>
2021-11-17 09:58:04 +0100

    stuff

---
+++ src/main/java/worldofzuul/GraphicsProcessor.java
@@ -0,0 +1,19 @@
+package worldofzuul;
+
+import java.util.ArrayList;
+import javafx.scene.image.Image;
+
+public class GraphicsProcessor {
+
+        private String graphicsDirectory = "/Graphics";
+        private String[] subDirectories = {"TestDirectory"};
+        private String[] testDirectory = {"TestPicture.png"};
+
+        public ArrayList<Image> getPicsForRoom(int i){
+
+
+
+
+        }
+
+}
